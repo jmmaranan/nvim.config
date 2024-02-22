25e603e6c5db1a5bb51280bbcb96b051f95a9a9c
Jose Maranan <jmmaranan@gmail.com>
2024-02-21 20:24:47 -0500

    Config yamlls for aws

---
+++ lua/plugins/lspconfig.lua
@@ -0,0 +1,29 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    settings = {
+      yaml = {
+        customTags = {
+          "!Base64",
+          "!Cidr",
+          "!FindInMap sequence",
+          "!GetAtt",
+          "!GetAZs",
+          "!ImportValue",
+          "!Join sequence",
+          "!Ref",
+          "!Select sequence",
+          "!Split sequence",
+          "!Sub sequence",
+          "!Sub",
+          "!And sequence",
+          "!Condition",
+          "!Equals sequence",
+          "!If sequence",
+          "!Not sequence",
+          "!Or sequence",
+        },
+      },
+    },
+  },
+}
